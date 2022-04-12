d6605059ac676c9901c5933f3d6887ef30246a7b
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-12 17:50:20 +0200

    lesson 6
    lesson 6
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- abc_site/pages/views.py
+++ abc_site/pages/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Digte fra sygesengen</h1>")
+    #return HttpResponse("<h1> Digte fra sygesengen</h1")
+    return render(request, 'pages/page.html')
