c7072ac9d4542918cc3428c0d1ec2994378d5966
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-12 20:46:48 +0200

    lesson 7
    lesson 7
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- abc_site/pages/views.py
+++ abc_site/pages/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from . models import Page
 #from django.http import HttpResponse
 
 
 # Create your views here.
-def index(request):
+def index(request, pagename):
+    pagename = '/' + pagename
+    pg = Page.objects.get(permalink=pagename)
+    context = {
+        'title': pg.title,
+        'content': pg.bodytext,
+        'last_updated': pg.update_date,
+    }
+    # assert False
     #return HttpResponse("<h1> Digte fra sygesengen</h1")
-    return render(request, 'pages/page.html')
+    return render(request, 'pages/page.html', context)
