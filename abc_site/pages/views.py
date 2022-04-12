8b0d44214db6c606bab1e1856cc7f25dd74a4edc
Your Name <90649822+LoneHilke@users.noreply.github.com>
2022-04-12 21:02:08 +0200

    lesson 7-2
    lesson 7-2
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- abc_site/pages/views.py
+++ abc_site/pages/views.py
@@ -11,6 +11,7 @@ def index(request, pagename):
         'title': pg.title,
         'content': pg.bodytext,
         'last_updated': pg.update_date,
+        'page_list': Page.objects.all(),
     }
     # assert False
     #return HttpResponse("<h1> Digte fra sygesengen</h1")
